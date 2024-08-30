430e86e6377a8f5895779d4a417a833414084362
ishikawa096 <ishikawa096@gmail.com>
2024-08-30 23:51:54 +0900

    add: http server

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"net/http"
 	"time"
 
 	"github.com/go-co-op/gocron/v2"
 )
+
+// HelloHandler は "Hello, World!" を返すエンドポイント
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	response := map[string]string{"message": "Hello, World!"}
+	fmt.Println("Request received")
+	json.NewEncoder(w).Encode(response)
+}
 
 func main() {
 	s, err := gocron.NewScheduler()
@@ -19,11 +29,12 @@ func main() {
 			10*time.Second,
 		),
 		gocron.NewTask(
-			func(a string, b int) {
-				fmt.Println("Hello, World!")
+			func(a string, b time.Time) {
+				// 現在時刻を出力
+				fmt.Println("Hello, World!", a, b)
 			},
 			"hello",
-			1,
+			time.Now(),
 		),
 	)
 	if err != nil {
@@ -36,12 +47,26 @@ func main() {
 	// start the scheduler
 	s.Start()
 
-	// block until you are ready to shut down
-	time.Sleep(time.Minute)
+	// HTTP サーバーの設定
+	http.HandleFunc("/hello", HelloHandler)
 
-	// when you're done, shut it down
-	err = s.Shutdown()
-	if err != nil {
-		// handle error
-	}
+	// ゴルーチンで HTTP サーバーを実行
+	go func() {
+		fmt.Println("HTTP server is running on port 8080")
+		if err := http.ListenAndServe(":8080", nil); err != nil {
+			fmt.Println("Error starting HTTP server:", err)
+		}
+	}()
+
+	// メインゴルーチンをブロック
+	select {}
+
+	// // block until you are ready to shut down
+	// time.Sleep(time.Minute)
+
+	// // when you're done, shut it down
+	// err = s.Shutdown()
+	// if err != nil {
+	// 	// handle error
+	// }
 }
