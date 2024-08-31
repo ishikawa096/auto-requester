d30cbc7be5d595bbb4604e43f9c1ec6bb3e640ed
ishikawa096 <ishikawa096@gmail.com>
2024-08-31 21:07:01 +0900

    add: handlers test

---
+++ handlers/stop_test.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestStopHandler(t *testing.T) {
+	req, err := http.NewRequest("GET", "/stop", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(StopHandler)
+
+	handler.ServeHTTP(rr, req)
+
+	// check the status code
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	// check response body
+	expected := map[string]string{"message": "job stop! To restart, please access /start"}
+	var actual map[string]string
+	if err := json.Unmarshal(rr.Body.Bytes(), &actual); err != nil {
+		t.Fatalf("could not unmarshal response: %v", err)
+	}
+
+	if actual["message"] != expected["message"] {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			actual["message"], expected["message"])
+	}
+}
